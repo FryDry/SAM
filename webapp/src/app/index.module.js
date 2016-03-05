343429577dc70a9f180af0e7c7f9233e40cbab62
Luca Condotta <luca.condotta@gmail.com>
2016-03-05 18:23:57 +0100

    Angular app in webapp

---
+++ webapp/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('myApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
